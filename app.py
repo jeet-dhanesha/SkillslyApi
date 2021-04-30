0b7d24c6114f63804487efcc596264c425288dae
Jeet Dhanesha <68452541+jeet-dhanesha@users.noreply.github.com>
2021-04-30 09:07:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def getScore():
                                                 "correctAnswerScore":0,
                                                 "fluencyScore": 0
                                 }
-                return string(e)
+                return str(e)
 
 if __name__ == '__main__': 
                 app.run(debug=True)
